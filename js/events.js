03fb9b78e63a220806a6b2afd2fbbaab68baef9d
Shuki Gur <rabbishuki@gmail.com>
2020-01-05 23:56:16 +0200

    first commit

---
+++ js/events.js
@@ -0,0 +1,48 @@
+const events = {
+  2: {
+    speakers: [
+      {img: 'ariel.jpeg', name: 'אריאל נחום', title: 'How to debug'},
+      {img: 'shai.jpeg', name: 'שי סלומון', title: 'Docker for life'},
+      {img: 'debate.jpg', name: 'פאנל מומחים', title: 'מדברים על הדברים הבוערים בהייטק'}
+    ],
+    footer: [
+      {
+        icon: 'location-icon.png',
+        text: ['בניין 770 כפר חב"ד', 'קומה שנייה']
+      },
+      {
+        icon: 'calendar-icon.png',
+        text: ['ליל שישי', 'י"ב אלול תשע"ט - 12/09/19']
+      },
+      {
+        icon: 'clock-icon.png',
+        text: ['18:30 התכנסות', '17:30 תחילת הרצאות']
+      }
+    ]
+  },
+  4: {
+    speakers: [
+      {img: 'rafi.jpeg', name: 'רפי דינרי', title: 'Protecting sensitive data in a Serverless DB'},
+      {img: 'avinoam.jpeg', name: 'אבינועם אהרוני', title: 'Full-stack development with WASM & Blazor'},
+      {img: 'ran.jpeg', name: 'רן בר זיק', title: 'Client Side Security'}
+    ],
+    footer: [
+      {
+        sponsor: true,
+        icon: 'microsoft.png',
+      },
+      {
+        icon: 'location-icon.png',
+        text: ['מייקרוסופט ריאקטור', 'רח\' דובנוב 7, תל אביב']
+      },
+      {
+        icon: 'calendar-icon.png',
+        text: ['ליל שישי', 'כ"ה שבט תש"פ - 20/02/20']
+      },
+      {
+        icon: 'clock-icon.png',
+        text: ['18:30 התכנסות', '17:30 תחילת הרצאות']
+      }
+    ]
+  }
+};
---
+++ js/main.js
@@ -0,0 +1,68 @@
+function getUrlVars() {
+  var vars = {};
+  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+    vars[key] = value;
+  });
+  return vars;
+}
+
+function createElementFromHTML(htmlString) {
+  var div = document.createElement('div');
+  div.innerHTML = htmlString.trim();
+
+  // Change this to div.childNodes to support multiple top-level nodes
+  return div.firstChild;
+}
+
+function main() {
+  const url = getUrlVars();
+  if (url && url.n && events[url.n]) {
+    updateTitle(url.n);
+    updateSpeakers(events[url.n].speakers);
+    updateFooter(events[url.n].footer);
+  } else {
+    console.error('invalid event')
+  }
+}
+
+function updateTitle(number) {
+  if (number) {
+    document.title += number;
+    document.getElementById('event-number').innerText += number;
+  }
+}
+
+function updateSpeakers(speakers) {
+  const speakerContainer = document.getElementById('speakers');
+
+  speakers.forEach((speaker) => {
+    const newSpeaker = createElementFromHTML(`<div>
+      <img class="speaker-image" src="img/${speaker.img}">
+      <p class="speaker-author">${speaker.name}</p>
+      <p class="speaker-title">${speaker.title}</p>
+    </div>`);
+    speakerContainer.append(newSpeaker);
+  });
+}
+
+function updateFooter(footer) {
+  const footerContainer = document.getElementById('footer');
+  footer.forEach(element => {
+    let newElement;
+    if (element.sponsor) {
+      newElement = createElementFromHTML(`<div class="container">
+        <img src="img/${element.icon}">
+       </div>`);
+    } else {
+      newElement = createElementFromHTML(`<div class="container">
+        <img class="icon" src="img/${element.icon}">
+        <address class="box">
+          <p>${element.text[0]}</p>
+          <p>${element.text[1]}</p>
+        </address>
+      </div>`);
+    }
+
+    footerContainer.append(newElement);
+  });
+}
